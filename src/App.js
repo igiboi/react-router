cfb0fe2bb3d3b8f9a6e30cc4d33788f548da9209
Luigi Matias <luigi.matias@codehunter.com>
2022-11-07 22:52:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Route } from 'react-router-dom';
+import Product from './pages/Product';
+import Welcome from './pages/Welcome';
+
+function App() {
+  return (
+    <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Product />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
